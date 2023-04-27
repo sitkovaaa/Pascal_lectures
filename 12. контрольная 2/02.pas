014b9b2f9c84b1d786973bbc0917eb6a07e38c8c
sitkovaaa <124240997+sitkovaaa@users.noreply.github.com>
2023-04-27 11:00:37 +0300

    Update 02.pas

--- 12. контрольная 2/02.pas
+++ 12. контрольная 2/02.pas
@@ -5,29 +5,54 @@ uses
     SysUtils, Math;
 
 const
-	Size_N = 190000000;
+  Size_N = 190000000;
 
 type
     arr700mb = array [0..Size_N] of integer;
     
     Array_obj = object
-		private
-			data : ^arr700mb;
-		public
-			constructor create_random();
-			destructor del();
+    private
+      data : ^arr700mb;
+    public
+      constructor create_random();
+      destructor del();
+      function get(index: integer): integer;
+      procedure set_val(index, value: integer);
     end;
+    
+constructor Array_obj.create_random();
+var
+  i: Integer;
+begin
+  Randomize;
+  New(data);
+  for i := Low(data^) to High(data^) do
+    data^[i] := Random(MaxInt);
+end;
 
+destructor Array_obj.del();
+begin
+  Dispose(data);
+end;
 
+function Array_obj.get(index: integer): integer;
+begin
+  Result := data^[index];
+end;
+
+procedure Array_obj.set_val(index, value: integer);
+begin
+  data^[index] := value;
+end;
 
 var
-	a : Array_obj;
-Begin
-	a.create_random;
-	a.create_random;
-	
-	a.del;
-	a.del;
-	
-	writeln('OK!')
-End.
+  a : Array_obj;
+begin
+  a.create_random;
+  a.set_val(0, 100); // Set the first element to 100
+  writeln(a.get(0)); // Output the first element
+  
+  a.del;
+  
+  writeln('OK!');
+end.
